82f108c29f672647a7b48d03618ff1b4c03c5f9e
NickRohan <n.raubs@gmail.com>
2023-01-18 16:00:04 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,398 @@
+//////////////////////////////////////////////////////////
+
+// Lesson 32, activating strict mode
+
+/*
+'use strict';
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log('I can drive.');
+
+const interface = 'Audio';
+*/
+
+// Lesson 33, functions
+
+/*
+'use strict';
+
+function logger() {
+    console.log('My name is Nick');
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+
+// Think of functions like little machines
+function soupMaker(pumpkins, carrots) {
+    const soup = `Soup with ${pumpkins} pumpkin and ${carrots} carrots.`;
+    return soup;
+}
+
+const pumpkinSoup = soupMaker(5, 0);
+console.log(pumpkinSoup);
+
+const pumpkinCarrotSoup = soupMaker(2, 4);
+console.log(pumpkinCarrotSoup);
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 34, function declarations vs expressions
+
+//Function declaration
+
+/*
+
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+
+const age1 = calcAge1(1992);
+console.log(age1);
+
+//Function Expression
+
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear
+}
+
+const age2 = calcAge2(1992);
+console.log(age1, age2);
+
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 35 - Arrow function
+
+/*
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear
+}
+
+// Arrow function
+const calcAge3 = birthYear => 2023 - birthYear
+const age3 = calcAge3(1992);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
+    // Return retirement
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1992, 'Nick'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 36 - Functions calling other functions
+/*
+
+const cutVegePieces = function (vege) {
+    return vege * 4;
+};
+
+const soupMaker = function (pumpkins, carrots) {
+
+    const pumpkinPieces = cutVegePieces(pumpkins);
+    const carrotPieces = cutVegePieces(carrots);
+
+    const soup = `Soup with ${pumpkinPieces} pieces of pumpkin and ${carrotPieces} pieces of carrots.`;
+    return soup;
+}
+
+console.log(soupMaker(2, 3));
+
+*/
+//////////////////////////////////////////////////////////
+
+// Lesson 37 - Reviewing functions
+/*
+const calcAge = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+
+        console.log(`${firstName} has already retired!`);
+        return -1;
+    }
+
+    // Return retirement
+    //
+}
+
+console.log(yearsUntilRetirement(1950, 'Nick'));
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 38 - Coding challenge #1
+
+/*
+'use strict';
+
+const calcAverage = (score1, score2, score3) => {
+    const average = (score1 + score2 + score3) / 3;
+    return average;
+}
+
+let dolphinsScore = calcAverage(44, 23, 71);
+let koalasScore = calcAverage(65, 54, 49);
+const koalas = 'Koalas';
+const dolphins = 'Dolphins';
+
+
+
+const firstScore = (dolphinsScore, koalasScore) => {
+    let higherScore = ''
+    let lowerScore = ''
+    if (dolphinsScore > koalasScore) {
+        higherScore = dolphinsScore
+        lowerScore = koalasScore
+    } else {
+        higherScore = koalasScore;
+        lowerScore = dolphinsScore;
+    }
+    return { higherScore, lowerScore };
+}
+
+const { higherScore, lowerScore } = firstScore(dolphinsScore, koalasScore);
+
+const checkWinner = (dolphinsScore, koalasScore) => {
+    let winner = ''
+    if (dolphinsScore >= (koalasScore * 2)) {
+        winner = dolphins
+    } else if (koalasScore >= (dolphinsScore * 2)) {
+        winner = koalas
+    } else {
+        winner = 'No team'
+    }
+    return winner;
+}
+
+const winner = checkWinner(dolphinsScore, koalasScore);
+
+
+
+// = function winningTeam(Koalas, Dolphins) {
+//     switch (winner) {
+//         case 'Koalas':
+//             break;
+//         case 'Dolphins':
+//             break;
+
+//     }
+
+
+// const dolphinsWin = (dolphinsScore > koalasScore)
+// const kaoalasWin = (dolphinsScore < koalasScore)
+// if (koalasWin) {
+//     console.log(`Koalas win ${koalasScore} vs ${dolphinsScore}`)
+// }
+
+// dolphinsScore = calcAverage(85, 54, 41);
+// koalasScore = calcAverage(23, 34, 27);
+
+
+
+console.log(`${winner} won ${higherScore} vs ${lowerScore}`)
+
+console.log(dolphinsScore, koalasScore);
+*/
+
+//////////////////////////////////////////////////////////
+
+/*
+
+// Lesson 39 - Introduction to Arrays
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+console.log(friends.length); // Retrieves the length of the array
+console.log(friends[2]);  // Retrieves the value at position 2 of the array
+
+const firstName = 'Nick';
+const nick = [firstName, 'Raubs', 2023 - 1992, 'webDev', friends];
+
+console.log(nick);
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2023 - birthYear;
+}
+const years = [1990, 1967, 2022, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+const age4 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3, age4);
+
+// Functions can be placed into arrays as they provide values
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+
+console.log(ages);
+
+*/
+
+//////////////////////////////////////////////////////////
+
+/*
+// Lesson 40 - Basic Array Operations
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Ad elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob')); // Will return -1 if item is not in the array
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend named Steven')
+}
+
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 41 - Coding challenge #2
+
+/*
+const calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        bill = bill * 0.15;
+    } else {
+        bill = bill * 0.20;
+    }
+    return bill;
+}
+
+// Instead of using if/else statements a ternary operator can be used
+
+// const calcTip = (bill) {
+//     return bill >= 50 && bill <= 300 ? 0.15 : bill * 0.2
+// }
+
+console.log(`${calcTip(125)}`);
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+
+console.log('Tips: ', tips[0], tips[1], tips[2]);
+console.log(`Total: $${total[0]}, $${total[1]}, $${total[2]}`)
+*/
+
+//////////////////////////////////////////////////////////
+
+// Lesson 42 - Introduction to objects
+
+/*
+
+const nickArray = [
+    'Nick',
+    'Raubs',
+    2023 - 1992,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+
+// Below is an object, objects are used to group variables together
+
+const nick = {
+    firstName: 'Nick',
+    lastName: 'Raubs',
+    age: 2023 - 1992,
+    job: 'Web devs',
+    friends: ['Michael', 'Peter', 'Steven']
+
+};
+*/
+
+//////////////////////////////////////////////////////////
+
+/*
+// Lesson 43 - Dots vs Bracket notation
+
+const nick = {
+    firstName: 'Nick',
+    lastName: 'Raubs',
+    age: 2023 - 1992,
+    job: 'Web dev',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(nick);
+
+console.log(nick.lastName);
+console.log(nick['lastName']);
+
+const nameKey = 'Name';
+console.log(nick['first' + nameKey]);
+console.log(nick['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Nick? Choose between firstName, lastName, age, job and friends');
+
+if (nick[interestedIn]) {
+    console.log(nick[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends')
+}
+
+nick.location = 'Staya';
+nick['twitter'] = '@nickrohan';
+console.log(nick);
+
+console.log(`${nick.firstName} has ${nick.friends.length} and his best
+friend is named ${nick.friends[0]}`)
+
+*/
+//////////////////////////////////////////////////////////
+
+// Lesson 44 - Object Methods
